dbbb316220248ce2af7598e7767981b4271036f3
Peter Metz <38048783+ppmetz@users.noreply.github.com>
2022-08-01 21:58:44 +0200

    add FilePath proj

---
+++ FilePath/FilePath.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace InspiredSoftware.Path;
+// inspired by "What I've learned from 20 years of programming in C#" with Joe Albahari
+public class FilePath : ICloneable, IComparable<FilePath>, IEquatable<FilePath>
+{
+    readonly string _FilePath;
+
+    public FilePath(string filePath) =>
+        _FilePath = GetValidated(filePath);
+
+    public static string GetValidated(string? filePath)
+    {
+        if (string.IsNullOrWhiteSpace(filePath)) throw new ArgumentNullException(nameof(filePath));
+
+        if (filePath.Length < 3) throw new ArgumentException(nameof(filePath));
+
+        if (System.IO.Path.GetInvalidPathChars().Intersect(filePath).Any()) throw new ArgumentException(nameof(filePath));
+
+        return System.IO.Path.GetFullPath(filePath.Trim());
+    }
+
+    public override string ToString() => _FilePath;
+
+    public static implicit operator FilePath(string filePath) => new(filePath);
+
+    public FilePath Combine(params string[] paths) =>
+        System.IO.Path.Combine(paths.Prepend(_FilePath).ToArray());
+
+    public virtual bool Equals(FilePath? other) =>
+            _FilePath.Equals(other?.ToString(), StringComparison.InvariantCultureIgnoreCase);
+
+    public object Clone() => new FilePath(_FilePath);
+
+    public int CompareTo(FilePath other) => (this.Equals(other)) 
+        ? 0
+        : _FilePath.CompareTo(other.ToString());
+}
